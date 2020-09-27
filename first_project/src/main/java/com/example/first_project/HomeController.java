72585e9e93e3aa1d1ea6f491e017a2d7f1ed203b
PANGSORAMDEPO <pangsoramdepo@gmail.com>
2020-09-27 14:43:19 +0700

    Initialize with Some Route and Noted

---
+++ first_project/src/main/java/com/example/first_project/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.first_project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    
+    @RequestMapping("/home")
+    public void Home()
+    {
+        System.out.println("Home Execute");
+    }
+}
